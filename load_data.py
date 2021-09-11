2b9428f8ded2c92ab1061a99b6848c0add0b6639
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-09-11 22:51:57 +0200

    init

---
+++ load_data.py
@@ -0,0 +1,34 @@
+import os
+
+import torchvision
+import torchvision.transforms as transforms
+from filelock import FileLock
+
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+
+def load_data(data_dir="./data"):
+    classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    with FileLock(os.path.expanduser("~/.data.lock")):
+        train_set = torchvision.datasets.CIFAR10(
+            root=data_dir, train=True, download=True, transform=transform)
+
+        test_set = torchvision.datasets.CIFAR10(
+            root=data_dir, train=False, download=True, transform=transform)
+
+    return train_set, test_set, classes
---
+++ test_best_model.py
@@ -0,0 +1,53 @@
+import neptune.new as neptune
+import torch
+
+from load_data import load_data
+from model import Net
+
+
+def test_best_model(run, best_trial):
+    best_trained_model = Net(best_trial.config["l1"], best_trial.config["l2"])
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    best_trained_model.to(device)
+
+    # (neptune) fetch project
+    project = neptune.get_project(name="common/project-hpo-with-ray-tune")
+
+    # (neptune) find best trial
+    best_run_df = project.fetch_runs_table(owner="kamil", tag="trial_run").to_pandas()
+    best_run_df = best_run_df.sort_values(by=["trial/metrics/valid/epoch/loss"])
+    best_run_id = best_run_df["sys/id"].values[0]
+
+    # (neptune) resume this run
+    best_run = neptune.init(
+        project="common/project-hpo-with-ray-tune",
+        run=best_run_id,
+        mode="read-only",
+    )
+
+    # (neptune) download model and close run
+    checkpoint_path = "./best_checkpoint"
+    best_run["trial/checkpoint"].download(checkpoint_path)
+    best_run.stop()
+
+    model_state, optimizer_state = torch.load(checkpoint_path)
+    best_trained_model.load_state_dict(model_state)
+
+    _, test_set, _ = load_data()
+
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=4, shuffle=False, num_workers=2)
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = best_trained_model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    # (neptune) log test accuracy
+    run["best/test_accuracy"] = correct / total
